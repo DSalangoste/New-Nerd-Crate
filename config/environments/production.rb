c4f25e91b6b40cc6e31710b19cddc9536e889135
Dale Salangoste <dalesalangoste@gmail.com>
2025-04-18 15:15:28 -0500

    Fix ActiveAdmin configuration and prepare for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.digest = true
+  config.assets.css_compressor = nil
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
