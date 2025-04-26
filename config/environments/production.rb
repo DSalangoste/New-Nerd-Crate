c0ea723e108b2dcc5b3e8b2e5929bf3ac950a8fa
Dale Salangoste <dalesalangoste@gmail.com>
2025-04-25 23:40:09 -0500

    Update Uglifier configuration for better ES6 support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,16 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.digest = true
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = Uglifier.new(
+    harmony: true,
+    compress: {
+      ecma: 2020,
+      warnings: false
+    },
+    mangle: {
+      keep_fnames: true
+    }
+  )
   config.assets.css_compressor = :sass
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
