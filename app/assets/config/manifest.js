c23e907bd7d91b8dd496c65c502c55c8b93cfcd4
Dale Salangoste <dalesalangoste@gmail.com>
2025-04-26 00:18:24 -0500

    Fix asset configuration and Bootstrap imports

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,6 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_directory ../javascripts .js
-//= link active_admin.css
 //= link application.css
 //= link application.js
 //= link controllers.js
-//= link vendor.js 
+//= link vendor.js
+//= link active_admin.css 
