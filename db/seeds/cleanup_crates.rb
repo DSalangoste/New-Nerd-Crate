f6cd34685583ee667e4bc8020a24e0689ba09d3c
Dale Salangoste <dalesalangoste@gmail.com>
2025-04-26 13:29:09 -0500

    Remove unnecessary cleanup script

--- db/seeds/cleanup_crates.rb
+++ db/seeds/cleanup_crates.rb
@@ -1,11 +1,17 @@
-# Remove crates without size designations
+# Remove crates without size designations (except Special category crates)
 puts "Removing crates without size designations..."
 
-# Find all crates that don't have a size designation (don't contain " - Small", " - Medium", or " - Large")
-regular_crates = CrateType.where("name NOT LIKE '% - Small' AND name NOT LIKE '% - Medium' AND name NOT LIKE '% - Large'")
+# Get the Special category
+special_category = Category.find_by(name: 'Special')
+
+# Find all crates that don't have a size designation and are not in the Special category
+regular_crates = CrateType.joins(:categories)
+                         .where("categories.name != 'Special'")
+                         .where("crate_types.name NOT LIKE '% - Small' AND crate_types.name NOT LIKE '% - Medium' AND crate_types.name NOT LIKE '% - Large'")
+                         .distinct
 
 if regular_crates.any?
-  puts "Found #{regular_crates.count} regular crates to remove:"
+  puts "Found #{regular_crates.count} non-special crates to remove:"
   regular_crates.each do |crate|
     puts "  - #{crate.name}"
     crate.destroy
